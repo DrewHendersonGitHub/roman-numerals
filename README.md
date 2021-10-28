4e40c059c68e15f1fabb6fd0dd72f10fc10aedde
Drew Henderson <drewhendersonemail@gmail.com>
2021-10-28 10:02:28 -0700

    passes first test converting to three I's
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+Test: "Inputting 1 - 3 will return the value in roman numerals"
+Code: convert(2);
+Expected Output: "II"
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-s
+function convert(number) {
+  let remainder =  0;
+  let numeral = "";
+  for (; number > 0; ) {
+    numeral = numeral.concat("I");
+    number--;
+  }
+  console.log(numeral);
+}
+
+let answer = convert(2);
+console.log(answer);
+
